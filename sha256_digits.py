665ef816465dbcbb45d47f83aa10373d039540f8
lusm <loveyousomuch554@gmail.com>
2024-05-14 22:54:44 +0300

    convert to utf8 before hash

--- sha256_digits.py
+++ sha256_digits.py
@@ -1,5 +1,6 @@
 import sys, hashlib
 assert len(sys.argv) == 2, 'As input should be filepath.'
 data_filepath = sys.argv[1]
-hash_digits = hashlib.sha256(open(data_filepath, 'rb').read()).hexdigest()
+file_data = open(data_filepath, 'rb').read().decode('utf-8', errors='ignore')
+hash_digits = hashlib.sha256(file_data.encode('utf-8')).hexdigest()
 print(hash_digits)
