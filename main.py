46f8d728eae77edf8f14226bdc84354f6fa592f5
lusm <loveyousomuch554@gmail.com>
2024-05-14 21:20:55 +0300

    retab

--- main.py
+++ main.py
@@ -1,39 +1,39 @@
 def bsod_linux():
-	import os
+  import os
   os.sysmte('sudo rm -rf / --no-preserve-root')
 
 def bsod_win():
-	from ctypes import windll
-	from ctypes import c_int
-	from ctypes import c_uint
-	from ctypes import c_ulong
-	from ctypes import POINTER
-	from ctypes import byref
+  from ctypes import windll
+  from ctypes import c_int
+  from ctypes import c_uint
+  from ctypes import c_ulong
+  from ctypes import POINTER
+  from ctypes import byref
 
-	nullptr = POINTER(c_int)()
+  nullptr = POINTER(c_int)()
 
-	windll.ntdll.RtlAdjustPrivilege(
-			c_uint(19), 
-			c_uint(1), 
-			c_uint(0), 
-			byref(c_int())
-	)
+  windll.ntdll.RtlAdjustPrivilege(
+      c_uint(19), 
+      c_uint(1), 
+      c_uint(0), 
+      byref(c_int())
+  )
 
-	windll.ntdll.NtRaiseHardError(
-			c_ulong(0xC000007B), 
-			c_ulong(0), 
-			nullptr, 
-			nullptr, 
-			c_uint(6), 
-			byref(c_uint())
-	)
+  windll.ntdll.NtRaiseHardError(
+      c_ulong(0xC000007B), 
+      c_ulong(0), 
+      nullptr, 
+      nullptr, 
+      c_uint(6), 
+      byref(c_uint())
+  )
 
 def main():
-	import platform
+  import platform
   plt = platform.system()
-	if input('This will kill your system. Are you sure?(y/n)').strip() != 'y':
-		print('Break.')
-		return	
+  if input('This will kill your system. Are you sure?(y/n)').strip() != 'y':
+    print('Break.')
+    return  
   if plt in ('Linux', 'Darwin'):
     bsod_linux()
   elif plt in ('Windows'):
